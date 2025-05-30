c73e65ac8d29ad4d5e453376e796a54430f8aaaa
Coen Dalby <coendalby@gmail.com>
2025-05-30 22:18:33 +0100

    Add hover-over comments to MorseManager

--- Project/Morse Key/include/MorseManager.h
+++ Project/Morse Key/include/MorseManager.h
@@ -10,17 +10,38 @@ enum MorseCode {
 class MorseManager
 {
 public:
+    /**
+     * @brief MorseManager constructor method.
+     */
     MorseManager();
+
+    /**
+     * @brief Gets the status of the button.
+     * @return Bool, true if button is pressed.
+     */
     bool ButtonPressed();
+
+    /**
+     * @brief Sets _buttonPressed to True and records time pressed.
+     */
     void ButtonPress();
+
+    /**
+     * @brief Sets _buttonPressed to False and calculates input symbol.
+     */
     void ButtonRelease();
+
+    /**
+     * @brief Gets the last input morse symbol.
+     * @return enum MorseCode, can be NONE, DOT, or DASH.
+     */
     MorseCode getLastInput(); 
 
 private:
-    bool _buttonPressed;
-    unsigned long _buttonDownTime;
-    unsigned long _buttonUpTime;
-    MorseCode _lastInput;
+    bool _buttonPressed;            // Current state of button; true if pressed.
+    unsigned long _buttonDownTime;  // Time button was last pressed down.
+    unsigned long _buttonUpTime;    // Time button was last released.
+    MorseCode _lastInput;           // The last input morse symbol.
     
 };
 
